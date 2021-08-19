516b0dede1b9b25badd1457700bf4da3c52e637e
Jose Mejia <mejialaguna@yahoo.com>
2021-08-19 16:49:46 -0400

    difininf and exporting manager clasees

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+// need to get some info from Employee file
+const Employee = require("./Employee");
+
+// The extends keyword is used in class declarations or class expressions to create a class that is a child of another class , his class should inherit from Employee.
+
+class Engineer extends Employee {
+  constructor(name, id, email, github) {
+    // When we call super(name), it passes the name argument to the
+    // constructor() of the Character class, where name and other
+    //  properties like health are officially defined. Afterwards,
+    //   the Player class will add any additional properties like this.i
+    //   nventory to the object.
+    super(name, id, email);
+    this.github = github;
+  }
+  getRole() {
+    return "Engineer";
+  }
+  getGithub() {
+    return this.github;
+  }
+}
+
+module.exports = Engineer;
