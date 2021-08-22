43f5439960c1caf0c15d9014709fe3434e8bf4b1
Jose Mejia <mejialaguna@yahoo.com>
2021-08-21 21:44:37 -0400

    adding validation to question

--- Develop/app.js
+++ Develop/app.js
@@ -12,60 +12,109 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 const employees = [];
 
-
-
 function createEmployee() {
-  inquirer.prompt([    
-    {
-      message: "what will be the Employee Role",
-      type: "list",
-      name: "role",
-      choices: ["Manager", "Engineer", "intern"]
-    }
-  ])
-  .then( data => { 
-    console.log(data);
-    if (data.role === "Manager") {
-      manager();
-    } else if (data.role === "Engineer") {
-      engineer();
-    } else  {
-      intern()
-    }
-  })
+  inquirer
+    .prompt([
+      {
+        message: "what will be the Employee Role",
+        type: "list",
+        name: "role",
+        choices: ["Manager", "Engineer", "intern"],
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+      if (data.role === "Manager") {
+        manager();
+      } else if (data.role === "Engineer") {
+        engineer();
+      } else {
+        intern();
+      }
+    });
 }
 function manager() {
   inquirer
-    .prompt([  
+    .prompt([
       {
         message: "Whats is the name of the new Employee",
         type: "input",
         name: "name",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log(
+              "please having a name its very important , please add one"
+            );
+            return false;
+          }
+        },
       },
       {
         message: "Employee ID Number",
         type: "input",
         name: "id",
+        validate: (nameInput) => {
+          // to make sure its a Number and no letters
+          if (isNaN(nameInput)) {
+            console.log("NO LETTERS , please add NUMBERS");
+            return false;
+          } else {
+            return true;
+          }
+        },
       },
       {
         message: "whats the Employee email",
         type: "input",
         name: "email",
-      },    
+        validate: (email) => {
+          if (email) {
+            // Regex mail check (return true if valid mail)
+            return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(
+              email
+            );
+          } else {
+            console.log("please add a valid email");
+            return false;
+          }
+        }
+      },
       {
         message: "manager Office number",
         type: "input",
         name: "officeNumber",
-      },    
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log(
+              "please having a name its very important , please add one"
+            );
+            return false;
+          }
+        },
+      },
       {
         message: "would you like to add another Employee",
         type: "confirm",
-        name: "confirmEmployee" 
-      }
+        name: "confirmEmployee",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log(
+              "please having a name its very important , please add one"
+            );
+            return false;
+          }
+        },
+      },
     ])
 
     .then((data) => {
-      console.log(data)
+      console.log(data);
       const manager = new Manager(
         data.name,
         data.id,
@@ -74,16 +123,16 @@ function manager() {
       );
       employees.push(manager);
 
-      if(data.confirmEmployee === true) {
-        createEmployee();      
-    } else{
-        createMember()
-    }
-  });
+      if (data.confirmEmployee === true) {
+        createEmployee();
+      } else {
+        createMember();
+      }
+    });
 }
 function engineer() {
   inquirer
-    .prompt([    
+    .prompt([
       {
         message: "Whats is the name of the new Employee",
         type: "input",
@@ -98,7 +147,7 @@ function engineer() {
         message: "whats the Employee email",
         type: "input",
         name: "email",
-      }, 
+      },
       {
         message: "Github username",
         type: "input",
@@ -108,10 +157,10 @@ function engineer() {
         message: "would you like to add another Employee",
         type: "confirm",
         name: "confirmEmployee",
-      }
+      },
     ])
     .then((data) => {
-      console.log(data)
+      console.log(data);
       const engineer = new Engineer(
         data.name,
         data.id,
@@ -120,16 +169,16 @@ function engineer() {
       );
       employees.push(engineer);
 
-      if(data.confirmEmployee === true){
-        createEmployee()
+      if (data.confirmEmployee === true) {
+        createEmployee();
       } else {
-        createMember()
+        createMember();
       }
     });
 }
 function intern() {
   inquirer
-    .prompt([  
+    .prompt([
       {
         message: "Whats is the name of the new Employee",
         type: "input",
@@ -144,42 +193,36 @@ function intern() {
         message: "whats the Employee email",
         type: "input",
         name: "email",
-      },    
+      },
       {
         message: "Where did the Intern went to school",
         type: "input",
-        name: "school"
+        name: "school",
       },
       {
         message: "would you like to add another Employee",
         type: "confirm",
-        name: "confirmEmployee"
-      }
+        name: "confirmEmployee",
+      },
     ])
     .then((data) => {
-      console.log(data)
-      const intern = new Intern(
-        data.name,
-        data.id,
-        data.email,
-        data.school      
-      );
+      console.log(data);
+      const intern = new Intern(data.name, data.id, data.email, data.school);
       employees.push(intern);
 
-      if(data.confirmEmployee === true){
-        createEmployee()
+      if (data.confirmEmployee === true) {
+        createEmployee();
       } else {
-        createMember()
+        createMember();
       }
     });
 }
 function createMember() {
-  fs.existsSync(OUTPUT_DIR) || fs.mkdirSync(OUTPUT_DIR)
-  
+  fs.existsSync(OUTPUT_DIR) || fs.mkdirSync(OUTPUT_DIR);
+
   fs.writeFileSync(outputPath, render(employees), "utf-8");
-  
-  console.log("created")
+
+  console.log("created");
 }
 
-createEmployee()
-
+createEmployee();
