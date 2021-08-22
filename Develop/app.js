ebdb874aeb374367cb51e3aa14ad2276ad08ef49
Jose Mejia <mejialaguna@yahoo.com>
2021-08-22 12:13:01 -0400

    fixing my code

--- Develop/app.js
+++ Develop/app.js
@@ -12,196 +12,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 const employees = [];
 
-// function createEmployee() {
-//   inquirer
-//     .prompt([
-//       {
-//         message: "what will be the Employee Role",
-//         type: "list",
-//         name: "role",
-//         choices: ["Manager", "Engineer", "intern"],
-//       },
-//       {
-//         message: "Whats is the name of the new Employee",
-//         type: "input",
-//         name: "name",
-//         validate: (nameInput) => {
-//           if (nameInput) {
-//             return true;
-//           } else {
-//             console.log(
-//               "please having a name its very important , please add one"
-//             );
-//             return false;
-//           }
-//         },
-//       },
-//       {
-//         message: "Employee ID Number",
-//         type: "input",
-//         name: "id",
-//         validate: (nameInput) => {
-//           // to make sure its a Number and no letters
-//           if (isNaN(nameInput)) {
-//             console.log("    NO LETTERS , please provide the correct number");
-//             return false;
-//           } else if (!nameInput) {
-//             console.log("please provide a EMPLOYEE ID NUMBER");
-//             return false;
-//           } else {
-//             return true;
-//           }
-//         },
-//       },
-//       {
-//         message: "whats the Employee email",
-//         type: "input",
-//         name: "email",
-//         validate: (email) => {
-//           if (!email) {
-//             console.log("please add a valid email");
-//             return false;
-//           } else {
-//             // Regex mail check (return true if valid mail)
-//             return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(
-//               email
-//             );
-//           }
-//         },
-//       }
-//     ])
-//     .then((data) => {
-//       console.log(data);
-//       if (data.role === "Manager") {
-//         manager();
-//       } else if (data.role === "Engineer") {
-//         engineer();
-//       } else {
-//         intern();
-//       }
-//     });
-// }
-// function manager() {
-//   inquirer
-//     .prompt([
-//       {
-//         message: "manager Office number",
-//         type: "input",
-//         name: "officeNumber",
-//         validate: (nameInput) => {
-//           if (isNaN(nameInput)) {
-//             console.log(
-//               "   office phone does not have letters!!!!!   please anter the proper numbers"
-//             );
-//             return false;
-//           } else if (!nameInput) {
-//             console.log("please add the OFFICE NUMBER");
-//             return false;
-//           } else {
-//             return true;
-//           }
-//         },
-//       },
-//       {
-//         message: "would you like to add another Employee",
-//         type: "confirm",
-//         name: "confirmEmployee",
-//       },
-//     ])
-
-//     .then((data) => {
-//       console.log(data);
-//       const manager = new Manager(
-//         data.name,
-//         data.id,
-//         data.email,
-//         data.officeNumber
-//       );
-//       employees.push(manager);
-
-//       if (data.confirmEmployee === true) {
-//         createEmployee();
-//       } else {
-//         createMember();
-//       }
-//     });
-// }
-// function engineer() {
-//   inquirer
-//     .prompt([
-//       {
-//         message: "Github username",
-//         type: "input",
-//         name: "gitHub",
-//         validate: (nameInput) => {
-//           if (nameInput) {
-//             return true;
-//           } else {
-//             console.log("please add you GITHUB user name");
-//             return false;
-//           }
-//         },
-//       },
-//       {
-//         message: "would you like to add another Employee",
-//         type: "confirm",
-//         name: "confirmEmployee",
-//       },
-//     ])
-//     .then((data) => {
-//       console.log(data);
-//       const engineer = new Engineer(
-//         data.name,
-//         data.id,
-//         data.email,
-//         data.gitHub
-//       );
-//       employees.push(engineer);
-
-//       if (data.confirmEmployee === true) {
-//         createEmployee();
-//       } else {
-//         createMember();
-//       }
-//     });
-// }
-// function intern() {
-//   inquirer
-//     .prompt([
-//       {
-//         message: "Where did the Intern went to school",
-//         type: "input",
-//         name: "school",
-//         validate: (nameInput) => {
-//           if (nameInput) {
-//             return true;
-//           } else {
-//             console.log("please provide your INTERN  school");
-//             return false;
-//           }
-//         },
-//       },
-//       {
-//         message: "would you like to add another Employee",
-//         type: "confirm",
-//         name: "confirmEmployee",
-//       },
-//     ])
-//     .then((data) => {
-//       console.log(data);
-//       const intern = new Intern(data.name, data.id, data.email, data.school);
-//       employees.push(intern);
-
-//       if (data.confirmEmployee === true) {
-//         createEmployee();
-//       } else {
-//         createMember();
-//       }
-//     });
-// }
-
-
-
 function createEmployee() {
   inquirer
     .prompt([
@@ -210,22 +20,7 @@ function createEmployee() {
         type: "list",
         name: "role",
         choices: ["Manager", "Engineer", "intern"],
-      }
-    ])
-    .then((data) => {
-      console.log(data);
-      if (data.role === "Manager") {
-        manager();
-      } else if (data.role === "Engineer") {
-        engineer();
-      } else {
-        intern();
-      }
-    });
-}
-function manager() {
-  inquirer
-    .prompt([
+      },
       {
         message: "Whats is the name of the new Employee",
         type: "input",
@@ -250,11 +45,11 @@ function manager() {
           if (isNaN(nameInput)) {
             console.log("    NO LETTERS , please provide the correct number");
             return false;
-          } else if (!nameInput){
+          } else if (!nameInput) {
             console.log("please provide a EMPLOYEE ID NUMBER");
             return false;
           } else {
-            return true
+            return true;
           }
         },
       },
@@ -266,41 +61,60 @@ function manager() {
           if (!email) {
             console.log("please add a valid email");
             return false;
-          } else  {
-              // Regex mail check (return true if valid mail)
-            return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+          } else {
+            // Regex mail check (return true if valid mail)
+            return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(
+              email
+            );
           }
-        }
-      },
+        },
+      }
+    ])
+    .then((data) => {
+      console.log(data);
+      if (data.role === "Manager") {
+        manager(data);
+      } else if (data.role === "Engineer") {
+        engineer(data);
+      } else {
+        intern(data);
+      }
+    });
+}
+function manager(previousAnswer) {
+  inquirer
+    .prompt([
       {
-        message: "manager Office number",
+        message: "What is the manager Office number?",
         type: "input",
         name: "officeNumber",
         validate: (nameInput) => {
           if (isNaN(nameInput)) {
-            console.log("   office phone does not have letters!!!!!   please anter the proper numbers")
+            console.log(
+              "   office phone does not have letters!!!!!   please enter the proper numbers"
+            );
             return false;
           } else if (!nameInput) {
             console.log("please add the OFFICE NUMBER");
             return false;
-          } else{
-            return true
+          } else {
+            return true;
           }
         },
       },
       {
         message: "would you like to add another Employee",
         type: "confirm",
-        name: "confirmEmployee"
+        name: "confirmEmployee",
       },
     ])
 
     .then((data) => {
       console.log(data);
       const manager = new Manager(
-        data.name,
-        data.id,
-        data.email,
+        previousAnswer.name,
+        previousAnswer.id,
+        previousAnswer.email,
         data.officeNumber
       );
       employees.push(manager);
@@ -312,64 +126,21 @@ function manager() {
       }
     });
 }
-function engineer() {
+function engineer(previousAnswer) {
   inquirer
     .prompt([
-      {
-        message: "Whats is the name of the new Employee",
-        type: "input",
-        name: "name",
-        validate: nameInput => {
-          if(nameInput){
-            return true;
-          } else{
-            console.log("knowing the name of your employees is very important , please provide one")
-            return false
-          }
-        }
-      },
-      {
-        message: "Employee ID Number",
-        type: "input",
-        name: "id",
-        validate: nameInput => {
-        if (isNaN(nameInput)){
-          console.log("  Please provide a number with no letters")
-          return false;
-        } else if (!nameInput) {
-          console.log("please provide a EMPLOYEE ID")
-          return false;
-        } else{
-          return true;
-        }
-        }
-      },
-      {
-        message: "whats the Employee email",
-        type: "input",
-        name: "email",
-        validate: (email) => {
-          if (!email) {
-            console.log("please add a valid email");
-            return false;
-          } else  {
-              // Regex mail check (return true if valid mail)
-            return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
-          }
-        }
-      },
       {
         message: "Github username",
         type: "input",
         name: "gitHub",
-        validate: nameInput => {
-          if(nameInput){
+        validate: (nameInput) => {
+          if (nameInput) {
             return true;
           } else {
-            console.log("please add you GITHUB user name")
-            return false
+            console.log("please add you GITHUB user name");
+            return false;
           }
-        }
+        },
       },
       {
         message: "would you like to add another Employee",
@@ -380,9 +151,9 @@ function engineer() {
     .then((data) => {
       console.log(data);
       const engineer = new Engineer(
-        data.name,
-        data.id,
-        data.email,
+        previousAnswer.name,
+        previousAnswer.id,
+        previousAnswer.email,
         data.gitHub
       );
       employees.push(engineer);
@@ -394,64 +165,21 @@ function engineer() {
       }
     });
 }
-function intern() {
+function intern(previousAnswer) {
   inquirer
     .prompt([
-      {
-        message: "Whats is the name of the new Employee",
-        type: "input",
-        name: "name",
-        validate: nameInput => {
-          if(nameInput){
-            return true;
-          } else {
-            console.log("knowing your EMPLOYEES NAME if very important")
-            return false;
-          }
-        }
-      },
-      {
-        message: "Employee ID Number",
-        type: "input",
-        name: "id",
-        validate: nameInput => {
-          if (isNaN(nameInput)){
-            console.log("  Please provide a real ID number!!!!! with no letters")
-            return false;
-          } else if (!nameInput) {
-            console.log("please provide a EMPLOYEE ID")
-            return false;
-          } else{
-            return true;
-          }
-          }
-      },
-      {
-        message: "whats the Employee email",
-        type: "input",
-        name: "email",
-        validate: (email) => {
-          if (!email) {
-            console.log("please add a valid email");
-            return false;
-          } else {
-              // Regex mail check (return true if valid mail)
-            return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
-          }
-        }
-      },
       {
         message: "Where did the Intern went to school",
         type: "input",
         name: "school",
-        validate : nameInput => {
-          if(nameInput){
+        validate: (nameInput) => {
+          if (nameInput) {
             return true;
           } else {
-            console.log("please provide your INTERN  school")
+            console.log("please provide your INTERN  school");
             return false;
           }
-        }
+        },
       },
       {
         message: "would you like to add another Employee",
@@ -461,7 +189,7 @@ function intern() {
     ])
     .then((data) => {
       console.log(data);
-      const intern = new Intern(data.name, data.id, data.email, data.school);
+      const intern = new Intern(previousAnswer.name, previousAnswer.id, previousAnswer.email, data.school);
       employees.push(intern);
 
       if (data.confirmEmployee === true) {
@@ -471,6 +199,7 @@ function intern() {
       }
     });
 }
+
 
 function createMember() {
   fs.existsSync(OUTPUT_DIR) || fs.mkdirSync(OUTPUT_DIR)
